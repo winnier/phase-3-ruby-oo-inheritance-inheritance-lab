cba9764c2e73a79a73b92417bef73f30ef084d28
Winnie Mendoza <wrm956@gmail.com>
2022-07-06 21:12:06 -0400

    commit tree d5dfdad269ce78872fc77b85f44896e463d54430

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+require_relative './user'
+class Student < User
+attr_accessor :knowledge
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
 end
